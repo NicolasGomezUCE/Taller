7eb7da42730d6f20a684fe25b345a5dfc9f94e75
WSP <wpenarandaa@ucentral.edu.co>
2024-03-30 22:09:16 -0500

    Modificación nombre package y clase.

--- src/main/java/co/edu/ucentral/taller/tallerIngSoftware/utilidades/Utilidad.java
+++ src/main/java/co/edu/ucentral/taller/tallerIngSoftware/utilidades/Utilidad.java
@@ -1,9 +1,9 @@
-package co.edu.ucentral.taller.tallerIngSoftware.Utilidades;
+package co.edu.ucentral.taller.tallerIngSoftware.utilidades;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class utilidad {
+public class Utilidad {
     public static String convertirFechaAString(Date fecha) {
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         return formato.format(fecha);
