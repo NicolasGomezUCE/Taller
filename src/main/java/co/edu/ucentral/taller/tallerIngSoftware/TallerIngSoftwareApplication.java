2df956f100c2ba4cc39fced9cb350d9004efc126
Nicolas <ngomezr2@ucentral.edu.co>
2024-03-30 21:08:43 -0500

    Añadir proyecto spring

---
+++ src/main/java/co/edu/ucentral/taller/tallerIngSoftware/TallerIngSoftwareApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.taller.tallerIngSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerIngSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerIngSoftwareApplication.class, args);
+	}
+
+}
